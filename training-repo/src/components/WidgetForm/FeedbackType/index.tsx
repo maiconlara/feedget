0d6374a9359d713959ff61945ea3bfccf5476f80
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-10 15:34:29 -0300

    fix: setFeedbackType in component

--- training-repo/src/components/WidgetForm/FeedbackType/index.tsx
+++ training-repo/src/components/WidgetForm/FeedbackType/index.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-
-
 interface FeedbackTypeProps {
   title: string;
   image: JSX.Element;
+  setFeedbackType: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-const FeedbackType = ({ title, image }: FeedbackTypeProps) => {
-  const [feedbackType, setFeedbackType] = useState<string | null>(null);
-
+const FeedbackType = ({ title, image, setFeedbackType }: FeedbackTypeProps) => {
   return (
     <button
       className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
--- training-repo/src/components/WidgetForm/index.tsx
+++ training-repo/src/components/WidgetForm/index.tsx
@@ -1,10 +1,11 @@
 import CloseButton from "../CloseButton";
 import { Bug, Lightbulb, Sparkle } from "phosphor-react";
 import FeedbackType from "./FeedbackType";
+import { useState } from "react";
 
 const WidgetForm = () => {
-  
-  
+  const [feedbackType, setFeedbackType] = useState<string | null>(null);
+  console.log("feedbackType", feedbackType);
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -13,14 +14,29 @@ const WidgetForm = () => {
       </header>
 
       <div className="flex py-8 gap-2 w-full">
-        <FeedbackType title="Problema" image={<Bug weight="bold" size={30} />} />
-        <FeedbackType title="Ideia" image={<Lightbulb weight="bold" size={30}/>} />
-        <FeedbackType title="Outro" image={<Sparkle weight="bold" size={30}/>} />
+        <FeedbackType
+          title="Problema"
+          image={<Bug weight="bold" size={30} />}
+          setFeedbackType={setFeedbackType}
+        />
+        <FeedbackType
+          title="Ideia"
+          image={<Lightbulb weight="bold" size={30} />}
+          setFeedbackType={setFeedbackType}
+        />
+        <FeedbackType
+          title="Outro"
+          image={<Sparkle weight="bold" size={30} />}
+          setFeedbackType={setFeedbackType}
+        />
       </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ por {""}
-        <a className="text-[#5ED3F3] hover:text-brand-500" href="https://github.com/maiconlara">
+        <a
+          className="text-[#5ED3F3] hover:text-brand-500"
+          href="https://github.com/maiconlara"
+        >
           Maicon Lara
         </a>
       </footer>
