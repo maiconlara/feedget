7f681e8547e1d05798c500d7c9cdbce2ea36db3d
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-10 16:44:53 -0300

    component: FeedbackContent

--- frontend/src/components/WidgetForm/FeedbackContent/index.tsx
+++ frontend/src/components/WidgetForm/FeedbackContent/index.tsx
@@ -1,13 +1,32 @@
+import { ArrowLeft } from "phosphor-react";
+import CloseButton from "../../CloseButton";
 
-
-
-
-const FeedbackContent = () => {
-    return (
-        
-        <div></div>
-
-    )
+interface FeedbackContentProps {
+  feedbackType: string;
+  handleRestartFeedback: () => void;
 }
+
+const FeedbackContent = ({
+  feedbackType,
+  handleRestartFeedback,
+}: FeedbackContentProps) => {
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={handleRestartFeedback}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6">{feedbackType}</span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
+};
 
 export default FeedbackContent;
--- frontend/src/components/WidgetForm/index.tsx
+++ frontend/src/components/WidgetForm/index.tsx
@@ -5,9 +5,17 @@ import { useState } from "react";
 const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<string | null>(null);
 
+  const handleRestartFeedback = () => {
+    setFeedbackType(null);
+  };
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? <FeedbackType setFeedbackType={setFeedbackType}/> : <FeedbackContent />}
+      {!feedbackType ? (
+        <FeedbackType setFeedbackType={setFeedbackType} />
+      ) : (
+        <FeedbackContent feedbackType={feedbackType} handleRestartFeedback={handleRestartFeedback}/>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ por {""}
