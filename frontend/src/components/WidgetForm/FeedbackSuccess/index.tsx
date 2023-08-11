46d02b49e012a3ef14159c12ecb1b927322a4945
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-11 10:54:48 -0300

    Feedback sucess button

--- frontend/src/components/WidgetForm/FeedbackSuccess/index.tsx
+++ frontend/src/components/WidgetForm/FeedbackSuccess/index.tsx
@@ -9,11 +9,18 @@ const FeedbackSuccess = () => {
       </header>
 
       <div className="flex flex-col items-center py-10 w-[304px]">
-        <img src={sucess} alt="sucesso" className="w-12 h-12" />
-      </div>
+        <img src={sucess} alt="sucesso" className="w-12 h-auto" />
+        <span className="text-xl mt-2 ">Agradeçemos seu Feedback!</span>
 
-      <span className="text-xl mt-8 ">Agradeçemos seu Feedback!</span>
-    </> 
+        <button
+          
+          type="button"
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+        >
+          Realizar outro
+        </button>
+      </div>
+    </>
   );
 };
 export default FeedbackSuccess;
