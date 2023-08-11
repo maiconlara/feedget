ee880a33c116e42b0e37b2e8d3da9a5b570ada1c
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-11 11:03:30 -0300

    Completed: feedget frontend

--- frontend/src/components/WidgetForm/FeedbackSuccess/index.tsx
+++ frontend/src/components/WidgetForm/FeedbackSuccess/index.tsx
@@ -1,7 +1,11 @@
 import CloseButton from "../../CloseButton";
 import sucess from "../../../assets/success.svg";
 
-const FeedbackSuccess = () => {
+interface FeedbackSucessProps {
+  onRestartFeedback: () => void;
+}
+
+const FeedbackSuccess = ({ onRestartFeedback }: FeedbackSucessProps) => {
   return (
     <>
       <header>
@@ -13,7 +17,7 @@ const FeedbackSuccess = () => {
         <span className="text-xl mt-2 ">Agradeçemos seu Feedback!</span>
 
         <button
-          
+          onClick={onRestartFeedback}
           type="button"
           className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
