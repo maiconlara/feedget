8d473ba5ea54d411fd4822f6eb96c3cac1267bfe
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-11 14:42:08 -0300

    feedback route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log("HTTP Server Running!");
 });
 
-app.get("/users", (req, res) => {
-  return res.send("hello world");
+
+app.post("/feedbacks", async (req, res) => {
+
+const { type, comment, screenshot } = req.body;
+ const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+  return res.status(201).json({data: feedback});
 });
