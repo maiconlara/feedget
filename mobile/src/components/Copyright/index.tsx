fbd5b5db09af74f9decdb1a80f43cd3a868ec5fe
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-15 14:02:16 -0300

    feedget type component

--- mobile/src/components/Copyright/index.tsx
+++ mobile/src/components/Copyright/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Copyright() {
   return (
-    <View style={styles.container}>
-      <Text>Blba </Text>
+    <View>
+      <Text style={styles.text}> Feito com ♥ por Maicon Lara </Text>
     </View>
   );
 }
